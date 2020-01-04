8eb9cb24982548dd635330d766a58f989ce82dd7
Jus2Good <jus2good@mac.com>
2020-01-04 01:27:16 -0500

    jan4demo

--- build/vendors.js
+++ build/vendors.js
@@ -43699,6 +43699,56 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_react__;
 
 /***/ }),
 
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-red/build/index.js":
+/*!*******************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Users/gatsby/Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-red/build/index.js ***!
+  \*******************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1074;
+        this.imageHeight = 2038;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-silver/build/index.js":
+/*!**********************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Users/gatsby/Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-silver/build/index.js ***!
+  \**********************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1074;
+        this.imageHeight = 2038;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
 /***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-spacegrey/build/index.js":
 /*!*************************************************************************************************************************************************************************************************************************************************!*\
   !*** /Users/gatsby/Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-spacegrey/build/index.js ***!
